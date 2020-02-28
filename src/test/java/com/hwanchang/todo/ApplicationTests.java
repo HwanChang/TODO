73ec9991afe349e02e5f93ddf2a0cbd3007c067f
HwanChang <ghksckd219@gmail.com>
2020-02-29 02:14:55 +0900

    프로젝트 생성

---
+++ src/test/java/com/hwanchang/todo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwanchang.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
