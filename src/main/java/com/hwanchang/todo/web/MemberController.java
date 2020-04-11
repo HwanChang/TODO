b5a06f77781cb4bb7e46d3de3b6dc7bfc80b6d9c
HwanChang <ghksckd219@gmail.com>
2020-04-11 23:06:46 +0900

    로그인, 인덱스 페이지 수정

--- src/main/java/com/hwanchang/todo/web/MemberController.java
+++ src/main/java/com/hwanchang/todo/web/MemberController.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,22 +18,23 @@ import javax.servlet.http.HttpServletRequest;
 @AllArgsConstructor
 public class MemberController {
 
-    private MemberService memberService;
+    private final MemberService memberService;
 
+    // 메인 페이지
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
+    // 회원 가입 페이지
     @GetMapping("/signup")
     public String signup( Model model, HttpServletRequest req ) {
-        model.addAttribute("message",req.getServletContext());
         return "signup";
     }
 
+    // 로그인 페이지
     @GetMapping("/signin")
     public String signin( Model model, HttpServletRequest req ) {
-        model.addAttribute("message",req.getServletContext());
         return "signin";
     }
 
